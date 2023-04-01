d274f1eb7037eb183a4b1f3ca656666d0daec343
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-01 13:11:28 +0600

    Rectangle draw input value C++

---
+++ Rectangular.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+int main () {
+  string M;
+
+  cout << "Enter: ";
+  cin >> M;
+
+  cout << M << M << M << M  << endl;
+  cout << M << M << M << M  << endl;
+  cout << M << M << M << M  << endl;
+  cout << M << M << M << M  << endl;
+  cout << M << M << M << M  << endl;
+  cout << M << M << M << M  << endl;
+
+
+
+  return 0;
+}
